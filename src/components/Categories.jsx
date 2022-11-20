5ae0bb0e556f8dfd027c5c1b03ef68e715fe51e3
BerdibekUralov <uralov0212@gmail.com>
2022-11-20 18:20:35 +0300

    react-router

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Categories({items, onClick}) {
+function Categories({items, onClickCategory}) {
+  const [activeItem, setActiveItem] = useState(null)
+
+  const onSelectItem = (index) => {
+    setActiveItem(index)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
+        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)} >Все</li>
         
         {items &&
           items.map((name, index) => (
             <li
+              className={activeItem === index ? 'active' : ''}
               key={`${name}_${index}`}
-              onClick={() => onClick(name)}  
+              onClick={() => onSelectItem(index)}
             >
               {name}
             </li>
